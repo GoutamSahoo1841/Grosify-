f2109e0ee6eb88f16de6305c22e3e69711231fe4
GoutamKumar1841 <goutamsahoogsst1841@gmail.com>
2026-02-03 21:53:23 +0530

    Update database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://kataricoder:katari@cluster0.cjku855.mongodb.net/grosify').then(() => console.log("DB Connected"))
+    await mongoose.connect('mongodb+srv://goutamsahoogsst1841_db_user:QMuyoVnjpFc14hRs@cluster0.hug1jpo.mongodb.net/?appName=Cluster0').then(() => console.log("DB Connected"))
 }
